65533341e38e5a5cf9312beebbfb4d7158425f5b
Darren Hendrickson <dub71ca@hotmail.com>
2021-01-22 14:16:29 -0600

    Original code merged with MERN boilerplate

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,38 +49,39 @@ exports.signup = (req, res) => {
 }
 
 exports.accountActivation = (req, res) => {
-    const { token } = req.body
-    if(token) {
-        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decodedToken) {
-            if(err) {
-                console.log('JWT VERIFY ACCOUNT ACTIVATION ERROR', err)
+    const { token } = req.body;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, function(err, decoded) {
+            if (err) {
+                console.log('JWT_VERIFY_IN_ACCOUNT_ACTIVATION_ERROR', err);
                 return res.status(401).json({
-                    error: 'Expired link. Try again'
-                })
+                    error: 'Expired link. Signup again'
+                });
             }
 
             const { name, email, password } = jwt.decode(token);
 
-            const user = new User({ name, email, password })
+            const user = new User({ name, email, password });
 
-            user.save((err, success) => {
-                if(err) {
-                    console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err)
+            user.save((err, user) => {
+                if (err) {
+                    console.log('SAVE_USER_IN_ACCOUNT_ACTIVATION_ERROR', err);
                     return res.status(401).json({
-                        error: 'Error saving user in database. Try signing up again.'
-                    })
+                        error: 'Error saving user in database. Try signup again'
+                    });
                 }
                 return res.json({
-                    message: 'Signup/Activation success'
+                    message: 'Signup success. Please signin.'
                 });
             });
         });
     } else {
         return res.json({
             message: 'Something went wrong. Try again.'
-        })
+        });
     }
-}
+};
 
 
 exports.signin = (req, res) => {
